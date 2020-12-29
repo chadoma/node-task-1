768b91ee2f1a197fdf6e308755102a3f2b90541d
chadoma <67055951+chadoma@users.noreply.github.com>
2020-12-29 12:14:59 +0900

    Revert "課題２"

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,21 @@
 import express from "express";
 
-import userRoutes from './routes/userRoutes';
-
-
+import userRoutes from './routes/users'
 const app = express();
 
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
-app.use("/public", express.static(__dirname + "/public/"));
+app.use("/public", express.static(__dirname + "/public/"))
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 const port = 3000;
 
 
-app.use('', userRoutes);
+app.use('', userRoutes)
 
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-});
+})
